17d7896f1187b625eb9f0bdfbde91b74f44eabd3
Ben Kremer <ben.kremer@hotmail.co.uk>
2015-07-16 00:41:22 +0200

    Initial commit

---
+++ app.js
@@ -0,0 +1,69 @@
+/*jshint node: true*/
+var path = require('path');
+var express = require('express');
+var app = express();
+var handlebars = require('express-handlebars').create({
+    defaultLayout: 'main',
+    helpers: {
+        section: function (name, options) {
+            if (!this._sections) this._sections = {}; // initialize _sections object
+            this._sections[name] = options.fn(this);
+            return null;
+        }
+    }
+});
+
+// Set up handlebars view engine
+app.set('views', path.join(__dirname, 'views'));
+app.engine('handlebars', handlebars.engine);
+app.set('view engine', 'handlebars');
+
+// Set up public directory for static files
+app.use(express.static('public'));
+
+// Set URL param 'test=1' for in-client testing
+app.use(function (req, res, next) {
+    res.locals.showTests = app.get('env') !== 'production' && req.query.test === '1';
+    next();
+});
+
+// Initialize & populate partials object for widgets
+app.use(function (req, res, next) {
+    if (!res.locals.partials) res.locals.partials = {};
+    next();
+});
+
+// Sets port to PORT environment variable or defaults to port 3000
+app.set('port', process.env.PORT || 3000);
+
+
+/**
+ * ROUTING
+ */
+
+// Landing Page
+app.get('/', function (req, res) {
+    res.render('index');
+});
+
+
+/**
+ * ERROR HANDLING 
+ */
+
+// 500 page
+app.use(function (err, req, res, next) {
+    console.error('Server encountered following error: ' + err);
+    res.status(500);
+    res.render('500');
+});
+
+// 404 page
+app.use(function (req, res, next) {
+    res.status(404);
+    res.render('404');
+});
+
+app.listen(app.get('port'), function () {
+    console.log('Express server started @ http://localhost:' + app.get('port'));
+});
