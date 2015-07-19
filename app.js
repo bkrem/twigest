38e9189760dffbe5225c5947bbf8a83481de8cf5
Ben Kremer <ben.kremer@hotmail.co.uk>
2015-07-19 22:09:21 +0200

    Adds Twitter Card template HTML/CSS

--- app.js
+++ app.js
@@ -6,14 +6,14 @@ var TweegestAction = require('./lib/tweegest');
 var express = require('express');
 var app = express();
 var handlebars = require('express-handlebars').create({
-    defaultLayout: 'main',
-    helpers: {
-        section: function (name, options) {
-            if (!this._sections) this._sections = {}; // initialize _sections object
-            this._sections[name] = options.fn(this);
-            return null;
-        }
-    }
+		defaultLayout: 'main',
+		helpers: {
+				section: function (name, options) {
+						if (!this._sections) this._sections = {}; // initialize _sections object
+						this._sections[name] = options.fn(this);
+						return null;
+				}
+		}
 });
 
 // Set up handlebars view engine
@@ -26,14 +26,14 @@ app.use(express.static('public'));
 
 // Set URL param 'test=1' for in-client testing
 app.use(function (req, res, next) {
-    res.locals.showTests = app.get('env') !== 'production' && req.query.test === '1';
-    next();
+		res.locals.showTests = app.get('env') !== 'production' && req.query.test === '1';
+		next();
 });
 
 // Initialize & populate partials object for widgets
 app.use(function (req, res, next) {
-    if (!res.locals.partials) res.locals.partials = {};
-    next();
+		if (!res.locals.partials) res.locals.partials = {};
+		next();
 });
 
 // Sets port to PORT environment variable or defaults to port 3000
@@ -46,9 +46,9 @@ app.set('port', process.env.PORT || 3000);
 
 // Landing Page
 app.get('/', function (req, res) {
-    res.render('index');
+		res.render('index');
 	var tweegest = new TweegestAction();
-	
+
 	//tweegest.getFriendIds('bkrem_', 20);
 	//tweegest.getFriendObjects('bkrem_');
 	tweegest.getVerifiedFriends('bkrem_', 100);
@@ -56,20 +56,20 @@ app.get('/', function (req, res) {
 
 
 /**
- * ERROR HANDLING 
+ * ERROR HANDLING
  */
 
 // 500 page
 app.use(function (err, req, res, next) {
-    console.error('Server encountered following error: ' + err);
-    res.status(500);
-    res.render('500');
+		console.error('Server encountered following error: ' + err);
+		res.status(500);
+		res.render('500');
 });
 
 // 404 page
 app.use(function (req, res, next) {
-    res.status(404);
-    res.render('404');
+		res.status(404);
+		res.render('404');
 });
 
 
@@ -77,8 +77,8 @@ app.use(function (req, res, next) {
  * SSL & SERVER INIT
  */
 var sslOptions = {
-    key: fs.readFileSync(__dirname + '/ssl/twigest.pem'),
-    cert: fs.readFileSync(__dirname + '/ssl/twigest.crt')
+		key: fs.readFileSync(__dirname + '/ssl/twigest.pem'),
+		cert: fs.readFileSync(__dirname + '/ssl/twigest.crt')
 };
 
 https.createServer(sslOptions, app).listen(app.get('port'), function () {
