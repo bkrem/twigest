04b2e237175711d14d552d4e3d03fffabc4386f3
Ben Kremer <ben.kremer@hotmail.co.uk>
2015-08-16 15:30:12 +0700

    Revert "Remove TODO"
    This reverts commit ba5cadcb0ce4565ac4ad045b1940f32f7a823eac.

--- public/js/main.js
+++ public/js/main.js
@@ -1,5 +1,6 @@
 $(document).ready(function () {
 
+    // Change btn styling and send AJAX GET '/trackid' on clicking "Track"
     $(function trackClick() {
         $('.card-container').on("click", function() {
             $(this).find('.track').css('background-color', '#34CF7A');
