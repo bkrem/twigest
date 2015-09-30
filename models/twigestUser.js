b69332c5062da95daf85152161ec0bd8dce1402e
Ben Kremer <ben.kremer@hotmail.co.uk>
2015-10-01 00:06:21 +0100

    Restructure model; add "user" key

--- models/twigestUser.js
+++ models/twigestUser.js
@@ -1,12 +1,18 @@
 var mongoose = require('mongoose');
 
 var twigestUserSchema = mongoose.Schema({
-    twitterId: { type: Number, required: true, unique: true, dropDups: true },
-    handle: String,
-    name: { type: String, required: true },
-    description: String, // TODO: Find solution for empty descriptions when required:true
-    verified: { type: Boolean, required: true },
-    topicTags: [String]
+    // TODO: Expand user object
+    user: {
+        handle: String
+    },
+    trackedUsers: {
+        twitterId: { type: Number, required: true, unique: true, dropDups: true },
+        handle: String,
+        name: { type: String, required: true },
+        description: String, // TODO: Find solution for empty descriptions when required:true
+        verified: { type: Boolean, required: true },
+        topicTags: [String]
+    }
 });
 
 var twigestUser = mongoose.model('twigestUser', twigestUserSchema);
